5deb4b3081e2594d3e3305baf888a4ed771d85c5
st-vantran <van.tran@supremetech.vn>
2022-08-01 15:56:41 +0700

    Add css + home page

---
+++ components/Article.vue
@@ -0,0 +1,98 @@
+<template>
+  <li class="article-item mb-3">
+    <div class="article pd-4">
+      <div class="article-header flex mb-2">
+        <RouterLink :to="`/user/profile/${article.userId}`">
+          <div class="article-meta flex">
+            <div class="article-author-img mr-2">
+              <img
+                :src="article?.user?.profile_image_90 || AVATAR_DEFAUL"
+                :alt="article?.user?.firstName"
+                class="author-img"
+              />
+            </div>
+            <div>
+              <p class="article-author-name">
+                {{ article?.user?.name }}
+              </p>
+              <time
+                class="article-time"
+                :dateTime="new Date().toISOString()"
+                title="Hôm nay đó"
+              >
+                {{ article?.published_at }}
+              </time>
+            </div>
+          </div>
+        </RouterLink>
+      </div>
+      <div class="article-body pl-10">
+        <h3 class="article-title mb-3">
+          <RouterLink :to="`/articles/${article?.id}`">{{
+            article?.title
+          }}</RouterLink>
+        </h3>
+        <p class="article-description end-line mb-2">
+          {{ article?.description }}
+        </p>
+        <div class="article-action fl-space-between">
+          <div class="article-action-left flex-center-y">
+            <div class="btn btn-flat-primary">
+              <RouterLink :to="`/articles/${article?.id}`">
+                <div class="flex-center-y">
+                  <i class="far fa-heart mr-2" />
+                  <span class="sm-hide">
+                    {{
+                      `${article?.positive_reactions_count} ${
+                        article?.positive_reactions_count > 1 ? 'likes' : 'like'
+                      }`
+                    }}
+                  </span>
+                  <span class="d-hide sm-visiable">{{ article?.positive_reactions_count }}</span>
+                </div>
+              </RouterLink>
+            </div>
+            <div class="btn btn-flat-primary">
+              <RouterLink :to="`/articles/${article?.id}`">
+                <div class="flex-center-y">
+                  <i class="far fa-comments mr-2" />
+                  <span class="sm-hide">
+                    {{
+                      `${article?.comments_count} ${
+                        article?.comments_count > 1 ? 'comments' : 'comment'
+                      }`
+                    }}
+                  </span>
+                  <span class="d-hide sm-visiable">{{
+                    article?.comments_count
+                  }}</span>
+                </div>
+              </RouterLink>
+            </div>
+          </div>
+          <div class="article-action-right">
+            <ButtonSave
+              onClick="{handleToggleBookmark}"
+              status="{isInBookmark}"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  // components: {
+  //   HeartIcon,
+  //   CommentsIcon,
+  // },
+  props: {
+    article: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,72 @@
 <template>
-  <Tutorial />
+  <div class="page-body home-page">
+    <section class="articles-section pd-4">
+      <div class="container">
+        <div class="row">
+          <div class="sm-hide col-2 col-md-3">
+            <SideBar />
+          </div>
+          <div class="col-7 col-md-9 col-sm-12">
+            <header class="fl-space-between mb-2">
+              <h2 class="section-title">Articles</h2>
+              <nav class="nav-tab sm-hide">
+                <ul class="flex nav-tab-list">
+                  <li class="nav-tab-item mx-1 pd-2 selected">
+                    <RouterLink to="/">All</RouterLink>
+                  </li>
+                  <li class="nav-tab-item mx-1 pd-2">
+                    <RouterLink to="/">Frontend</RouterLink>
+                  </li>
+                  <li class="nav-tab-item mx-1 pd-2">
+                    <RouterLink to="/">Backend</RouterLink>
+                  </li>
+                  <li class="nav-tab-item mx-1 pd-2">
+                    <RouterLink to="/">Database</RouterLink>
+                  </li>
+                </ul>
+              </nav>
+              <select class="crayon-select d-hide sm-visiable">
+                <option value="all">All</option>
+                <option value="Frontend">Frontend</option>
+                <option value="Backend">Backend</option>
+                <option value="Database">Database</option>
+              </select>
+            </header>
+            <template>
+              <ul
+                class="articles-list"
+                v-for="(article, index) in articles"
+                :key="index"
+              >
+                <Article :article="article" />
+              </ul>
+            </template>
+          </div>
+          <SideBarRight />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
-export default Vue.extend({
-  name: 'IndexPage',
-})
+export default {
+  async fetch() {
+    const articles = await fetch(
+      `https://dev.to/api/articles?tag=nuxt&state=rising&page=${this.currentPage}`
+    ).then((res) => res.json());
+
+    this.articles = this.articles.concat(articles);
+    console.log(this.articles);
+  },
+
+  data() {
+    return {
+      currentPage: 1,
+      articles: [],
+    };
+  },
+};
 </script>
